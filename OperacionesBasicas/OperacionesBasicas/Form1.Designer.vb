8fa5120b9eb94d912fe6022e636af80ee029ca69
Andrés Domínguez <andres_gomez@live.com.mx>
2017-10-10 15:26:27 -0600

    Operaciones basicas

---
+++ OperacionesBasicas/OperacionesBasicas/Form1.Designer.vb
@@ -0,0 +1,414 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculadora
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.gb1 = New System.Windows.Forms.GroupBox()
+        Me.lblresultadosuma = New System.Windows.Forms.Label()
+        Me.lblprevresultado = New System.Windows.Forms.Label()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.gb2 = New System.Windows.Forms.GroupBox()
+        Me.lblresultadoresta = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnresultado2 = New System.Windows.Forms.Button()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.txt4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.gb3 = New System.Windows.Forms.GroupBox()
+        Me.txtbox7 = New System.Windows.Forms.TextBox()
+        Me.lbl9 = New System.Windows.Forms.Label()
+        Me.btnresultado3 = New System.Windows.Forms.Button()
+        Me.txt5 = New System.Windows.Forms.TextBox()
+        Me.txt6 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.gb4 = New System.Windows.Forms.GroupBox()
+        Me.txt8 = New System.Windows.Forms.TextBox()
+        Me.lbl10 = New System.Windows.Forms.Label()
+        Me.btnresultado4 = New System.Windows.Forms.Button()
+        Me.txt9 = New System.Windows.Forms.TextBox()
+        Me.txt10 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.gb1.SuspendLayout()
+        Me.gb2.SuspendLayout()
+        Me.gb3.SuspendLayout()
+        Me.gb4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gb1
+        '
+        Me.gb1.Controls.Add(Me.lblresultadosuma)
+        Me.gb1.Controls.Add(Me.lblprevresultado)
+        Me.gb1.Controls.Add(Me.btn1)
+        Me.gb1.Controls.Add(Me.txt2)
+        Me.gb1.Controls.Add(Me.txt1)
+        Me.gb1.Controls.Add(Me.lbl2)
+        Me.gb1.Controls.Add(Me.lbl1)
+        Me.gb1.Location = New System.Drawing.Point(12, 12)
+        Me.gb1.Name = "gb1"
+        Me.gb1.Size = New System.Drawing.Size(272, 139)
+        Me.gb1.TabIndex = 0
+        Me.gb1.TabStop = False
+        Me.gb1.Text = "Suma"
+        '
+        'lblresultadosuma
+        '
+        Me.lblresultadosuma.AutoSize = True
+        Me.lblresultadosuma.Location = New System.Drawing.Point(75, 84)
+        Me.lblresultadosuma.Name = "lblresultadosuma"
+        Me.lblresultadosuma.Size = New System.Drawing.Size(0, 13)
+        Me.lblresultadosuma.TabIndex = 6
+        '
+        'lblprevresultado
+        '
+        Me.lblprevresultado.AutoSize = True
+        Me.lblprevresultado.Location = New System.Drawing.Point(14, 84)
+        Me.lblprevresultado.Name = "lblprevresultado"
+        Me.lblprevresultado.Size = New System.Drawing.Size(55, 13)
+        Me.lblprevresultado.TabIndex = 5
+        Me.lblprevresultado.Text = "Resultado"
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(68, 100)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(141, 23)
+        Me.btn1.TabIndex = 4
+        Me.btn1.Text = "CALCULAR"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(129, 58)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(121, 20)
+        Me.txt2.TabIndex = 3
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(129, 30)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(121, 20)
+        Me.txt1.TabIndex = 2
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(14, 61)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(88, 13)
+        Me.lbl2.TabIndex = 1
+        Me.lbl2.Text = "Ingresa el valor 2"
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(14, 33)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(88, 13)
+        Me.lbl1.TabIndex = 0
+        Me.lbl1.Text = "Ingresa el valor 1"
+        '
+        'gb2
+        '
+        Me.gb2.Controls.Add(Me.lblresultadoresta)
+        Me.gb2.Controls.Add(Me.Label8)
+        Me.gb2.Controls.Add(Me.btnresultado2)
+        Me.gb2.Controls.Add(Me.txt3)
+        Me.gb2.Controls.Add(Me.txt4)
+        Me.gb2.Controls.Add(Me.Label3)
+        Me.gb2.Controls.Add(Me.Label4)
+        Me.gb2.Location = New System.Drawing.Point(307, 12)
+        Me.gb2.Name = "gb2"
+        Me.gb2.Size = New System.Drawing.Size(272, 139)
+        Me.gb2.TabIndex = 5
+        Me.gb2.TabStop = False
+        Me.gb2.Text = "Resta"
+        '
+        'lblresultadoresta
+        '
+        Me.lblresultadoresta.AutoSize = True
+        Me.lblresultadoresta.Location = New System.Drawing.Point(77, 84)
+        Me.lblresultadoresta.Name = "lblresultadoresta"
+        Me.lblresultadoresta.Size = New System.Drawing.Size(0, 13)
+        Me.lblresultadoresta.TabIndex = 7
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(16, 84)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(55, 13)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "Resultado"
+        '
+        'btnresultado2
+        '
+        Me.btnresultado2.Location = New System.Drawing.Point(78, 100)
+        Me.btnresultado2.Name = "btnresultado2"
+        Me.btnresultado2.Size = New System.Drawing.Size(141, 23)
+        Me.btnresultado2.TabIndex = 4
+        Me.btnresultado2.Text = "CALCULAR"
+        Me.btnresultado2.UseVisualStyleBackColor = True
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(129, 58)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(121, 20)
+        Me.txt3.TabIndex = 3
+        '
+        'txt4
+        '
+        Me.txt4.Location = New System.Drawing.Point(129, 30)
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(121, 20)
+        Me.txt4.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Ingresa el valor 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(14, 33)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Ingresa el valor 1"
+        '
+        'gb3
+        '
+        Me.gb3.Controls.Add(Me.txtbox7)
+        Me.gb3.Controls.Add(Me.lbl9)
+        Me.gb3.Controls.Add(Me.btnresultado3)
+        Me.gb3.Controls.Add(Me.txt5)
+        Me.gb3.Controls.Add(Me.txt6)
+        Me.gb3.Controls.Add(Me.Label5)
+        Me.gb3.Controls.Add(Me.Label6)
+        Me.gb3.Location = New System.Drawing.Point(12, 161)
+        Me.gb3.Name = "gb3"
+        Me.gb3.Size = New System.Drawing.Size(272, 163)
+        Me.gb3.TabIndex = 6
+        Me.gb3.TabStop = False
+        Me.gb3.Text = "Multiplicación"
+        '
+        'txtbox7
+        '
+        Me.txtbox7.Enabled = False
+        Me.txtbox7.Location = New System.Drawing.Point(129, 86)
+        Me.txtbox7.Name = "txtbox7"
+        Me.txtbox7.Size = New System.Drawing.Size(121, 20)
+        Me.txtbox7.TabIndex = 8
+        '
+        'lbl9
+        '
+        Me.lbl9.AutoSize = True
+        Me.lbl9.Location = New System.Drawing.Point(14, 93)
+        Me.lbl9.Name = "lbl9"
+        Me.lbl9.Size = New System.Drawing.Size(55, 13)
+        Me.lbl9.TabIndex = 7
+        Me.lbl9.Text = "Resultado"
+        '
+        'btnresultado3
+        '
+        Me.btnresultado3.Location = New System.Drawing.Point(68, 127)
+        Me.btnresultado3.Name = "btnresultado3"
+        Me.btnresultado3.Size = New System.Drawing.Size(141, 23)
+        Me.btnresultado3.TabIndex = 4
+        Me.btnresultado3.Text = "CALCULAR"
+        Me.btnresultado3.UseVisualStyleBackColor = True
+        '
+        'txt5
+        '
+        Me.txt5.Location = New System.Drawing.Point(129, 58)
+        Me.txt5.Name = "txt5"
+        Me.txt5.Size = New System.Drawing.Size(121, 20)
+        Me.txt5.TabIndex = 3
+        '
+        'txt6
+        '
+        Me.txt6.Location = New System.Drawing.Point(129, 30)
+        Me.txt6.Name = "txt6"
+        Me.txt6.Size = New System.Drawing.Size(121, 20)
+        Me.txt6.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(14, 61)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 13)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Ingresa el valor 2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(14, 33)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(88, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Ingresa el valor 1"
+        '
+        'gb4
+        '
+        Me.gb4.Controls.Add(Me.txt8)
+        Me.gb4.Controls.Add(Me.lbl10)
+        Me.gb4.Controls.Add(Me.btnresultado4)
+        Me.gb4.Controls.Add(Me.txt9)
+        Me.gb4.Controls.Add(Me.txt10)
+        Me.gb4.Controls.Add(Me.Label11)
+        Me.gb4.Controls.Add(Me.Label12)
+        Me.gb4.Location = New System.Drawing.Point(307, 161)
+        Me.gb4.Name = "gb4"
+        Me.gb4.Size = New System.Drawing.Size(272, 163)
+        Me.gb4.TabIndex = 9
+        Me.gb4.TabStop = False
+        Me.gb4.Text = "División"
+        '
+        'txt8
+        '
+        Me.txt8.Enabled = False
+        Me.txt8.Location = New System.Drawing.Point(129, 86)
+        Me.txt8.Name = "txt8"
+        Me.txt8.Size = New System.Drawing.Size(121, 20)
+        Me.txt8.TabIndex = 8
+        '
+        'lbl10
+        '
+        Me.lbl10.AutoSize = True
+        Me.lbl10.Location = New System.Drawing.Point(14, 93)
+        Me.lbl10.Name = "lbl10"
+        Me.lbl10.Size = New System.Drawing.Size(55, 13)
+        Me.lbl10.TabIndex = 7
+        Me.lbl10.Text = "Resultado"
+        '
+        'btnresultado4
+        '
+        Me.btnresultado4.Location = New System.Drawing.Point(68, 127)
+        Me.btnresultado4.Name = "btnresultado4"
+        Me.btnresultado4.Size = New System.Drawing.Size(141, 23)
+        Me.btnresultado4.TabIndex = 4
+        Me.btnresultado4.Text = "CALCULAR"
+        Me.btnresultado4.UseVisualStyleBackColor = True
+        '
+        'txt9
+        '
+        Me.txt9.Location = New System.Drawing.Point(129, 58)
+        Me.txt9.Name = "txt9"
+        Me.txt9.Size = New System.Drawing.Size(121, 20)
+        Me.txt9.TabIndex = 3
+        '
+        'txt10
+        '
+        Me.txt10.Location = New System.Drawing.Point(129, 30)
+        Me.txt10.Name = "txt10"
+        Me.txt10.Size = New System.Drawing.Size(121, 20)
+        Me.txt10.TabIndex = 2
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(14, 61)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(88, 13)
+        Me.Label11.TabIndex = 1
+        Me.Label11.Text = "Ingresa el valor 2"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(14, 33)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(88, 13)
+        Me.Label12.TabIndex = 0
+        Me.Label12.Text = "Ingresa el valor 1"
+        '
+        'Calculadora
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(597, 346)
+        Me.Controls.Add(Me.gb4)
+        Me.Controls.Add(Me.gb3)
+        Me.Controls.Add(Me.gb2)
+        Me.Controls.Add(Me.gb1)
+        Me.Name = "Calculadora"
+        Me.Text = "Calculadora básica"
+        Me.gb1.ResumeLayout(False)
+        Me.gb1.PerformLayout()
+        Me.gb2.ResumeLayout(False)
+        Me.gb2.PerformLayout()
+        Me.gb3.ResumeLayout(False)
+        Me.gb3.PerformLayout()
+        Me.gb4.ResumeLayout(False)
+        Me.gb4.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gb1 As GroupBox
+    Friend WithEvents lblresultadosuma As Label
+    Friend WithEvents lblprevresultado As Label
+    Friend WithEvents btn1 As Button
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents gb2 As GroupBox
+    Friend WithEvents btnresultado2 As Button
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents gb3 As GroupBox
+    Friend WithEvents btnresultado3 As Button
+    Friend WithEvents txt5 As TextBox
+    Friend WithEvents txt6 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtbox7 As TextBox
+    Friend WithEvents lbl9 As Label
+    Friend WithEvents gb4 As GroupBox
+    Friend WithEvents txt8 As TextBox
+    Friend WithEvents lbl10 As Label
+    Friend WithEvents btnresultado4 As Button
+    Friend WithEvents txt9 As TextBox
+    Friend WithEvents txt10 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents lblresultadoresta As Label
+End Class
---
+++ OperacionesBasicas/OperacionesBasicas/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Calculadora
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Dim suma1 As Integer = Val(txt1.Text)
+        Dim suma2 As Integer = Val(txt2.Text)
+        Dim resultado As Integer = suma1 + suma2
+        lblresultadosuma.Text = resultado
+    End Sub
+
+    Private Sub btnresultado2_Click(sender As Object, e As EventArgs) Handles btnresultado2.Click
+        Dim var1 As Integer = Val(txt4.Text)
+        Dim var2 As Integer = Val(txt3.Text)
+        Dim resultado As Integer = var1 - var2
+        lblresultadoresta.Text = resultado
+    End Sub
+
+    Private Sub btnresultado3_Click(sender As Object, e As EventArgs) Handles btnresultado3.Click
+        Dim var1 As Integer = Val(txt6.Text)
+        Dim var2 As Integer = Val(txt5.Text)
+        Dim resultado As Integer = var1 * var2
+        txtbox7.Text = resultado
+    End Sub
+
+    Private Sub btnresultado4_Click(sender As Object, e As EventArgs) Handles btnresultado4.Click
+        Dim var1 As Integer = Val(txt10.Text)
+        Dim var2 As Integer = Val(txt9.Text)
+        Dim resultado As Integer = var1 / var2
+        txt8.Text = resultado
+    End Sub
+End Class
