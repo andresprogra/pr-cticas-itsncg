20f15babc4da1f19a61af36c53664208446754fc
Andrés Domínguez <andres_gomez@live.com.mx>
2017-10-12 16:08:43 -0600

    Termine el programa de promedio

--- Practica3Promedio/Practica3Promedio/Form1.Designer.vb
+++ Practica3Promedio/Practica3Promedio/Form1.Designer.vb
@@ -22,9 +22,209 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtunidadi = New System.Windows.Forms.TextBox()
+        Me.txtunidadii = New System.Windows.Forms.TextBox()
+        Me.txtunidadiii = New System.Windows.Forms.TextBox()
+        Me.txtunidadiv = New System.Windows.Forms.TextBox()
+        Me.txtunidadv = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtresult = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblresult = New System.Windows.Forms.Label()
+        Me.lblmssg = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(209, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(172, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "PROMEDIO DE CALIFICACIONES"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(213, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Fundamentos  de programación"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 82)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Unidad I"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(33, 108)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Unidad II"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(33, 134)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Unidad III"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(33, 160)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(54, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Unidad IV"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(33, 186)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Unidad V"
+        '
+        'txtunidadi
+        '
+        Me.txtunidadi.Location = New System.Drawing.Point(117, 75)
+        Me.txtunidadi.Name = "txtunidadi"
+        Me.txtunidadi.Size = New System.Drawing.Size(191, 20)
+        Me.txtunidadi.TabIndex = 7
+        '
+        'txtunidadii
+        '
+        Me.txtunidadii.Location = New System.Drawing.Point(117, 101)
+        Me.txtunidadii.Name = "txtunidadii"
+        Me.txtunidadii.Size = New System.Drawing.Size(191, 20)
+        Me.txtunidadii.TabIndex = 8
+        '
+        'txtunidadiii
+        '
+        Me.txtunidadiii.Location = New System.Drawing.Point(117, 127)
+        Me.txtunidadiii.Name = "txtunidadiii"
+        Me.txtunidadiii.Size = New System.Drawing.Size(191, 20)
+        Me.txtunidadiii.TabIndex = 9
+        '
+        'txtunidadiv
+        '
+        Me.txtunidadiv.Location = New System.Drawing.Point(117, 153)
+        Me.txtunidadiv.Name = "txtunidadiv"
+        Me.txtunidadiv.Size = New System.Drawing.Size(191, 20)
+        Me.txtunidadiv.TabIndex = 10
+        '
+        'txtunidadv
+        '
+        Me.txtunidadv.Location = New System.Drawing.Point(117, 179)
+        Me.txtunidadv.Name = "txtunidadv"
+        Me.txtunidadv.Size = New System.Drawing.Size(191, 20)
+        Me.txtunidadv.TabIndex = 11
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(388, 116)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 48)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "GENERAR PROMEDIO"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtresult
+        '
+        Me.txtresult.Location = New System.Drawing.Point(376, 220)
+        Me.txtresult.Name = "txtresult"
+        Me.txtresult.Size = New System.Drawing.Size(100, 20)
+        Me.txtresult.TabIndex = 13
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(316, 227)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(54, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Promedio:"
+        '
+        'lblresult
+        '
+        Me.lblresult.AutoSize = True
+        Me.lblresult.Location = New System.Drawing.Point(33, 227)
+        Me.lblresult.Name = "lblresult"
+        Me.lblresult.Size = New System.Drawing.Size(86, 13)
+        Me.lblresult.TabIndex = 15
+        Me.lblresult.Text = "Tu promedio es: "
+        '
+        'lblmssg
+        '
+        Me.lblmssg.AutoSize = True
+        Me.lblmssg.Location = New System.Drawing.Point(33, 257)
+        Me.lblmssg.Name = "lblmssg"
+        Me.lblmssg.Size = New System.Drawing.Size(0, 13)
+        Me.lblmssg.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(529, 289)
+        Me.Controls.Add(Me.lblmssg)
+        Me.Controls.Add(Me.lblresult)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtresult)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtunidadv)
+        Me.Controls.Add(Me.txtunidadiv)
+        Me.Controls.Add(Me.txtunidadiii)
+        Me.Controls.Add(Me.txtunidadii)
+        Me.Controls.Add(Me.txtunidadi)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtunidadi As TextBox
+    Friend WithEvents txtunidadii As TextBox
+    Friend WithEvents txtunidadiii As TextBox
+    Friend WithEvents txtunidadiv As TextBox
+    Friend WithEvents txtunidadv As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtresult As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblresult As Label
+    Friend WithEvents lblmssg As Label
 End Class
--- Practica3Promedio/Practica3Promedio/Form1.vb
+++ Practica3Promedio/Practica3Promedio/Form1.vb
@@ -1,3 +1,38 @@
 ﻿Public Class Form1
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
 
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim uniti, unitii, unitiii, unitiv, unitv, suma, promedio As Decimal
+        uniti = txtunidadi.Text
+        unitii = txtunidadii.Text
+        unitiii = txtunidadiii.Text
+        unitiv = txtunidadiv.Text
+        unitv = txtunidadv.Text
+        suma = uniti + unitii + unitiii + unitiv + unitv
+        promedio = suma / 5
+        MsgBox("El promedio obtenido es: " & promedio)
+        lblresult.Text = lblresult.Text & promedio
+        txtresult.Text = promedio
+        showMessage(promedio)
+    End Sub
+
+    Private Sub showMessage(promedio As Decimal)
+        If promedio < 70 Then
+            lblmssg.Text = "Reprobado, repetición de curso."
+        ElseIf promedio >= 70 And promedio < 80 Then
+            lblmssg.Text = "Necesitas estudiar más, es importante la investigación y práctica."
+        ElseIf promedio >= 80 And promedio < 90 Then
+            lblmssg.Text = "Eres un buen estudiante, obtuviste una calificación buena."
+        ElseIf promedio >= 90 And promedio <= 100 Then
+            lblmssg.Text = "Eres un excelente estudiante, sigue así por el orgullo de ser búfalo."
+        Else
+            lblmssg.Text = "Error..."
+        End If
+    End Sub
 End Class
