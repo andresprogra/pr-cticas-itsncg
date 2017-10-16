144106949ecf802ef3ab3f00f4527f479e599e9d
Andrés Domínguez <andres_gomez@live.com.mx>
2017-10-16 15:41:58 -0600

    login finish

---
+++ LoginPractica4/LoginPractica4/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "usuario" And TextBox2.Text = "pass" Then
+            Form2.Show()
+            Me.Hide()
+        Else
+            MsgBox("Login incorrecto")
+        End If
+    End Sub
+End Class
